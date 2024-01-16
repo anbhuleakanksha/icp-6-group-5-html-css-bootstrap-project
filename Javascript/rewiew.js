9ed7105eae378c4c5d0647d8546048ca3b484ce4
kavitavarade <kavitavarade40@gmail.com>
2024-01-16 07:11:04 -0800

    file added

---
+++ Javascript/rewiew.js
@@ -0,0 +1,10 @@
+
+          function sConsole()
+           {
+            var data = document.getElementById("data");
+            console.log(data.value); 
+            var data = document.getElementById("data1");
+            console.log(data1.value);
+          }
+          //sConsole(); This would call it only on script load, which isn't what you want
+    
